d1f578133aa63c9e9fd56ce7a284077873e3d130
Oli351 <os351@kent.ac.uk>
2024-10-18 09:36:03 +0000

    final

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,12 @@
+variable = input("phrase in camel case: ")
 
+def change_case(camel_str):
+   snake_str = ""
+   for letter in camel_str:
+      if letter.isupper():
+         snake_str += "_" + letter.lower()
+      else:
+         snake_str += letter
+   return snake_str
+
+print(change_case(variable))
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,12 @@
+total = 0
+
+while total < 75:
+    coin_value = int(input("value of coin: "))
+    print("please add more coins")
+    total += coin_value
+
+if total >= 75:
+    print("Tank you!")
+    change = total - 75
+    print(f"your change is", change)
 
